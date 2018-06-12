cff9f88834ab992f1f989f27d129ddc6b0a4a246
tatatxi <tatatxi@gmail.com>
2018-06-11 21:10:35 -0300

    espero que funcione

---
+++ TrabalhoBianca/TrabalhoBianca/Aluno.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoBianca
+{
+    public class Aluno
+    {
+        public int Codigo { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public DateTime DataNasc { get; set; }
+
+        public int idade()
+        {
+            int idadeAluno = DateTime.Today.Year - this.DataNasc.Year;
+
+            return idadeAluno;
+        }
+
+        public string nomeCompleto()
+        {
+            return string.Concat(Nome + Sobrenome);
+        }
+
+        public int validaNome()
+        {
+            int i = 0;
+            string n = Nome;
+            string s = Sobrenome;
+
+            if (n.Contains("Ç") || n.Contains("ç") || s.Contains("Ç") || s.Contains("ç"))
+            {
+                i = +1;
+            }
+            if (n.Contains("@") || s.Contains("@"))
+            {
+                
+                i = +2;
+            }
+            return i;
+        }
+
+        public string erroValidacao(int i)
+        {
+            string erro;
+            if (validaNome() == 1)
+            {
+                erro = "Este nome não pode ser usado contém caracter 'Ç' que é inválido no inicio do nome";
+                return erro;
+            }
+            else if (validaNome() == 2)
+            {
+                erro = "Este nome não pode ser usado contém caracter '@' que é inválido";
+                return erro;
+            }
+
+            return "";
+        }
+
+    }
+}
---
+++ TrabalhoBianca/TrabalhoTeste/TesteAluno.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrabalhoBianca;
+
+namespace TrabalhoTeste
+{
+    [TestClass]
+    public class TesteAluno
+    {
+
+        [TestMethod]
+        public void TestedeIdadePositivo()
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Codigo = 1;
+            a1.Nome = "Felipe";
+            a1.Sobrenome = "Vozgeral Çanca";
+            a1.DataNasc = new DateTime(1990,04,01);
+
+            Aluno a2 = new Aluno();
+
+            a2.Codigo = 2;
+            a2.Nome = "Alin@";
+            a2.Sobrenome = "Branca";
+            a2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno a3 = new Aluno();
+
+            a3.Codigo = 3;
+            a3.Nome = "Alfredo";
+            a3.Sobrenome = "Sella III";
+            a3.DataNasc = new DateTime(1986, 03, 12);
+
+            Assert.IsTrue(a1.idade() > 18);
+
+        }
+
+        [TestMethod]
+        public void TestedeIdadeNegativo()
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Codigo = 1;
+            a1.Nome = "Felipe";
+            a1.Sobrenome = "Vozgeral Çanca";
+            a1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno a2 = new Aluno();
+
+            a2.Codigo = 2;
+            a2.Nome = "Alin@";
+            a2.Sobrenome = "Branca";
+            a2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno a3 = new Aluno();
+
+            a3.Codigo = 3;
+            a3.Nome = "Alfredo";
+            a3.Sobrenome = "Sella III";
+            a3.DataNasc = new DateTime(1986, 03, 12);
+
+            Assert.IsTrue(a2.idade() > 0);
+
+        }
+
+        [TestMethod]
+        public void TestedeNomePositivo()
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Codigo = 1;
+            a1.Nome = "Felipe";
+            a1.Sobrenome = "Vozgeral Çanca";
+            a1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno a2 = new Aluno();
+
+            a2.Codigo = 2;
+            a2.Nome = "Alin@";
+            a2.Sobrenome = "Branca";
+            a2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno a3 = new Aluno();
+
+            a3.Codigo = 3;
+            a3.Nome = "Alfredo";
+            a3.Sobrenome = "Sella III";
+            a3.DataNasc = new DateTime(1986, 03, 12);
+
+            Assert.AreEqual(a1.Nome, "Felipe");
+
+        }
+
+        [TestMethod]
+        public void TestedeNomeNegativo()
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Codigo = 1;
+            a1.Nome = "Felipe";
+            a1.Sobrenome = "Vozgeral Çanca";
+            a1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno a2 = new Aluno();
+
+            a2.Codigo = 2;
+            a2.Nome = "Alin@";
+            a2.Sobrenome = "Branca";
+            a2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno a3 = new Aluno();
+
+            a3.Codigo = 3;
+            a3.Nome = "Alfredo";
+            a3.Sobrenome = "Sella III";
+            a3.DataNasc = new DateTime(1986, 03, 12);
+
+            Assert.AreNotEqual(a2.nomeCompleto(),a2.Nome+a2.Sobrenome);
+
+        }
+
+        [TestMethod]
+        public void TestedeValidaNomePositivo()
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Codigo = 1;
+            a1.Nome = "Felipe";
+            a1.Sobrenome = "Vozgeral Çanca";
+            a1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno a2 = new Aluno();
+
+            a2.Codigo = 2;
+            a2.Nome = "Alin@";
+            a2.Sobrenome = "Branca";
+            a2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno a3 = new Aluno();
+
+            a3.Codigo = 3;
+            a3.Nome = "Alfredo";
+            a3.Sobrenome = "Sella III";
+            a3.DataNasc = new DateTime(1986, 03, 12);
+
+            Assert.AreNotEqual(a1.Sobrenome, a1.validaNome());
+
+        }
+
+        [TestMethod]
+        public void TestedeValidaNomeNegativo()
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Codigo = 1;
+            a1.Nome = "Felipe";
+            a1.Sobrenome = "Vozgeral Çanca";
+            a1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno a2 = new Aluno();
+
+            a2.Codigo = 2;
+            a2.Nome = "Alin@";
+            a2.Sobrenome = "Branca";
+            a2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno a3 = new Aluno();
+
+            a3.Codigo = 3;
+            a3.Nome = "Alfredo";
+            a3.Sobrenome = "Sella III";
+            a3.DataNasc = new DateTime(1986, 03, 12);
+
+            a1.validaNome();
+
+            Assert.IsTrue(a1.validaNome() < 0);
+
+        }
+
+        [TestMethod]
+        public void TestedeErroValidacaoPositivo()
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Codigo = 1;
+            a1.Nome = "Felipe";
+            a1.Sobrenome = "Vozgeral Çanca";
+            a1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno a2 = new Aluno();
+
+            a2.Codigo = 2;
+            a2.Nome = "Alin@";
+            a2.Sobrenome = "Branca";
+            a2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno a3 = new Aluno();
+
+            a3.Codigo = 3;
+            a3.Nome = "Alfredo";
+            a3.Sobrenome = "Sella III";
+            a3.DataNasc = new DateTime(1986, 03, 12);
+
+            Assert.AreSame("Este nome não pode ser usado contém caracter 'Ç' que é inválido no inicio do nome", a1.erroValidacao(a1.validaNome()));
+
+        }
+
+        [TestMethod]
+        public void TestedeErroValidacaoNegativo()
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Codigo = 1;
+            a1.Nome = "Felipe";
+            a1.Sobrenome = "Vozgeral Çanca";
+            a1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno a2 = new Aluno();
+
+            a2.Codigo = 2;
+            a2.Nome = "Alin@";
+            a2.Sobrenome = "Branca";
+            a2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno a3 = new Aluno();
+
+            a3.Codigo = 3;
+            a3.Nome = "Alfredo";
+            a3.Sobrenome = "Sella III";
+            a3.DataNasc = new DateTime(1986, 03, 12);
+
+
+            Assert.AreSame("Este nome não pode ser usado contém caracter", a1.erroValidacao(a1.validaNome()));
+
+
+        }
+
+    }
+}
